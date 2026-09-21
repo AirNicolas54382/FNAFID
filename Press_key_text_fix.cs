2f058111e66e3b8539a0376aff4c2af44e081cd9
Mikolaj <108267340+AirNicolas54382@users.noreply.github.com.>
2026-09-21 13:27:50 +0200

    chore: resolve text display issues and configure engine

---
+++ Press_key_text_fix.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Press_key_text_fix : MonoBehaviour
+{
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // text.SetActive(false);
+    }
+}
